a3f1487bfb949024060fc09264de9833b3f12455
Guillaume Fouilleul <fouinux@gmail.com>
2024-02-29 16:57:28 +0100

    Serial: Fix clock source

--- gameboy/serial.c
+++ gameboy/serial.c
@@ -9,6 +9,12 @@
 #include "mem.h"
 #include "irq.h"
 
+enum serial_clock_select
+{
+    CLOCK_EXTERNAL = 0,
+    CLOCK_INTERNAL = 1,
+};
+
 struct serial_t serial;
 
 void serial_init(void)
@@ -26,24 +32,27 @@ void serial_exec(void)
 
     if ((serial.clock_cnt & 0x7F) == 0x00)
     {
-        if (serial.pReg->SC_Flags.TransferEnable == 1)
+        if (serial.pReg->SC_Flags.ClockSelect == CLOCK_INTERNAL)
         {
-            // Output buffer
-            serial.output_buffer <<= 1;
-            if (serial.pReg->SB & 0x80)
-                serial.output_buffer |= 1;
-
-            serial.pReg->SB <<= 1;
-            serial.pReg->SB |= 0x01; // Disconnected
-            serial.xfer_cnt++;
-            if (serial.xfer_cnt == 8)
+            if (serial.pReg->SC_Flags.TransferEnable == 1)
             {
-                serial.pReg->SC_Flags.TransferEnable = 0;
-                serial.xfer_cnt = 0;
-                irq.pIF->Flags.Serial = 1;
+                // Output buffer
+                serial.output_buffer <<= 1;
+                if (serial.pReg->SB & 0x80)
+                    serial.output_buffer |= 1;
 
-                putchar(serial.output_buffer);
-                fflush(stdout);
+                serial.pReg->SB <<= 1;
+                serial.pReg->SB |= 0x01; // Disconnected
+                serial.xfer_cnt++;
+                if (serial.xfer_cnt == 8)
+                {
+                    serial.pReg->SC_Flags.TransferEnable = 0;
+                    serial.xfer_cnt = 0;
+                    irq.pIF->Flags.Serial = 1;
+
+                    putchar(serial.output_buffer);
+                    fflush(stdout);
+                }
             }
         }
     }
